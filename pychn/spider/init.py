09d49ca0fd5fe59cf5151f268b728d24fd0a45ea
imagecser <lestdawn@163.com>
2017-10-26 16:43:35 +0800

    recursion spider

--- pychn/spider/init.py
+++ pychn/spider/init.py
@@ -12,13 +12,19 @@ url = "http://www.matrix67.com/"
 scrawled_sites = []
 
 pages = set()
+header = {
+    'User-Agent': 'Baiduspider+(+http://www.baidu.com/search/spider.htm)'
+}
 
-def get_links(site):
+
+def get_links(site, depth):
+    if depth > 3:
+        return
     global pages
     try:
-        html = requests.get(site, timeout=3).text
-    except:
-        print('timeout: ' + site)
+        html = requests.get(site, headers=header, timeout=3).text
+    except Exception as e:
+        print(e)
         return
     soup = BeautifulSoup(html, 'lxml')
     for script in soup(['script', 'style']):
@@ -26,15 +32,16 @@ def get_links(site):
     for link in soup.find_all('a'):
         if 'href' in link.attrs:
             href = link.attrs['href']
+            if re.match(url, href):
+                pass
+            elif re.match(r'http', href):
+                continue
+            else:
+                href = url + href
             if href not in pages:
-                if re.match(url, href):
-                    pass
-                elif re.match(r'http', href):
-                    continue
-                else:
-                    href = url + href
                 print(href)
                 pages.add(href)
-                get_links(href)
+                get_links(href, depth + 1)
 
-get_links(url)
+
+get_links(url, 0)
