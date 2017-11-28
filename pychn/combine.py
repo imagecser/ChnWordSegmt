d145be4b22a77b8799da33eba19e31f3aa555fe5
imagecser <lestdawn@163.com>
2017-11-28 11:46:54 +0800

    new way

--- pychn/combine.py
+++ pychn/combine.py
@@ -6,16 +6,16 @@ def update(i):
     engine = sqlalchemy.create_engine("mysql+pymysql://root:root@localhost/chn?charset=utf8", echo=True)
     engine.execute("create table in" + str(i) + " character set utf8 collate utf8_unicode_ci(select * from test limit " + str(i - 1) + "000000, 1000000);");
     engine.execute("create table com" + str(i) + "(select word, sum(frequency) as frequency, group_concat(prefix, ',') as prefix, group_concat(suffix, ',') as suffix from in"  + str(i) + " group by word);")
-    engine.execute("insert into com" + str(i) + "(word, frequency, prefix, suffix) select word, frequency, prefix, suffix from out" + str(i -2) + ";")
-    engine.execute("create table out" + str(i - 1) + "(select word, sum(frequency) as frequency, group_concat(prefix, ',') as prefix, group_concat(suffix, ',') as suffix from com" + str(i) + " group by word);")
-    engine.execute("drop table in" + str(i) + ", com" + str(i) + ", out" + str(i-2))
+    engine.execute("insert into com" + str(i) + "(word, frequency, prefix, suffix) select word, frequency, prefix, suffix from out" + str(i -1) + ";")
+    engine.execute("create table out" + str(i) + "(select word, sum(frequency) as frequency, group_concat(prefix, ',') as prefix, group_concat(suffix, ',') as suffix from com" + str(i) + " group by word);")
+    engine.execute("drop table in" + str(i) + ", com" + str(i) + ", out" + str(i - 1))
 
 def clean(i):
     i = int(i)
     engine = sqlalchemy.create_engine("mysql+pymysql://root:root@localhost/chn?charset=utf8", echo=True)
-    engine.execute("delete from out" + str(i - 1) + " where char_length(word) > 2 and frequency = 1;");
+    engine.execute("delete from out" + str(i) + " where char_length(word) > 2 and frequency = 1;");
 
-for i in range(42, 50):
+for i in range(48, 51):
     update(i)
     if i % 3 == 0:
         clean(i)
